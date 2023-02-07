1869b9097b38bc9375900d48a029f126461c7ba0
OleksandrDanchuk <117369013+OleksandrDanchuk@users.noreply.github.com>
2023-02-07 21:07:17 +0200

    start

---
+++ js/2mod.js
@@ -0,0 +1,56 @@
+// function checkAge(age) {
+//   if (age >= 18) {
+//     return 'You are an adult';
+//   }
+//   return 'You are a minor';
+// }
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   if (password === ADMIN_PASSWORD) {
+//     return 'Welcome!';
+//   }
+//   return 'Access denied, wrong password!';
+// }
+
+// function checkStorage(available, ordered) {
+//   if (ordered === 0) {
+//     return 'Your order is empty!';
+//   }
+//   if (ordered > available) {
+//     return 'Your order is too large, not enough goods in stock!';
+//   }
+//   return 'The order is accepted, our manager will contact you';
+// }
+
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits[3];
+
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+// fruits[1] = 'peach';
+// fruits[3] = 'banana';
+
+// const fruits = ['apple', 'peach', 'pear', 'banana'];
+// const fruitsArrayLength = fruits.length;
+
+// const fruits = ['apple', 'peach', 'pear', 'banana'];
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+
+// function getExtremeElements(array) {
+//   return [array[0], array[array.length - 1]];
+// }
+
+// function splitMessage(message, delimiter) {
+//   let words;
+//   return message.split(delimiter);
+//   return words;
+// }
+
+// function calculateEngravingPrice(message, pricePerWord) {
+//   return message.split(' ').length * pricePerWord;
+// }
