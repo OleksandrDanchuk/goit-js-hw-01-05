9d46fb04064a6111c10bfb561923a2e89bf7cf2a
OleksandrDanchuk <117369013+OleksandrDanchuk@users.noreply.github.com>
2023-03-16 15:27:42 +0200

    halfway

--- js/4mod.js
+++ js/4mod.js
@@ -46,3 +46,158 @@
 // pizzaPalace.order('Four meats', makePizza, onOrderError);
 // pizzaPalace.order('Big Mike', makePizza, onOrderError);
 // pizzaPalace.order('Vienna', makePizza, onOrderError);
+
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   orderedItems.forEach(function (element) {
+//     totalPrice += element;
+//   });
+//   return totalPrice;
+// }
+
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+//     numbers.forEach(function (element) {
+//         if (element > value) {
+//             filteredNumbers.push(element);
+//         }
+//     });
+// //   for (let i = 0; i < numbers.length; i += 1) {
+// //     if (numbers[i] > value) {
+// //       filteredNumbers.push(numbers[i]);
+// //     }
+//   return filteredNumbers;
+// }
+
+// function getCommonElements(firstArray, secondArray) {
+//   const commonElements = [];
+//   firstArray.forEach(function (element) {
+//     if (secondArray.includes(element)) {
+//       commonElements.push(element);
+//     }
+//   });
+//   for (let i = 0; i < firstArray.length; i += 1) {
+//     if (secondArray.includes(firstArray[i])) {
+//       commonElements.push(firstArray[i]);
+//     }
+//   }
+//   return commonElements;
+// }
+
+// const calculateTotalPrice = (quantity, pricePerItem) => {
+//   return quantity * pricePerItem;
+// };
+
+// const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+
+// const calculateTotalPrice = orderedItems => {
+//   let totalPrice = 0;
+//   orderedItems.forEach(item => {
+//     totalPrice += item;
+//   });
+//   return totalPrice;
+// };
+
+// const filterArray = (numbers, value) => {
+//   const filteredNumbers = [];
+//   numbers.forEach(number => {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+//   return filteredNumbers;
+// };
+
+// const getCommonElements = (firstArray, secondArray) => {
+//   const commonElements = [];
+//   firstArray.forEach(element => {
+//     if (secondArray.includes(element)) {
+//       commonElements.push(element);
+//     }
+//   });
+//   return commonElements;
+// };
+
+// function changeEven(numbers, value) {
+//    const newArray = [];
+//    numbers.forEach(number => {
+//      if (number % 2 === 0) {
+//        number += value;
+//      }
+//      newArray.push(number);
+//    });
+//    return newArray;
+// }
+
+// const planets = ['Earth', 'Mars', 'Venus', 'Jupiter'];
+// const planetsLengths = planets.map(planet => planet.length);
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+// ];
+// const titles = books.map(book => book.title);
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     genres: ['adventure', 'history'],
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     genres: ['fiction'],
+//   },
+//   {
+//     title: 'Redder Than Blood',
+//     author: 'Tanith Lee',
+//     genres: ['horror', 'mysticism'],
+//   },
+// ];
+// const genres = books.flatMap(book => book.genres);
+
+// const getUserNames = users => {
+//   return users.map(user => user.name);
+// };
+
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+// const oddNumbers = numbers.filter(number => number % 2 !== 0);
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     genres: ['adventure', 'history'],
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     genres: ['fiction', 'mysticism'],
+//   },
+//   {
+//     title: 'Redder Than Blood',
+//     author: 'Tanith Lee',
+//     genres: ['horror', 'mysticism', 'adventure'],
+//   },
+// ];
+// const allGenres = books.flatMap(book => book.genres);
+// const uniqueGenres = allGenres.filter(
+//   (genres, index, array) => array.indexOf(genres) === index
+// );
