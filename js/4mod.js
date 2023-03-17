9d29f88b170ae15128d2115455c89b043c633a0e
OleksandrDanchuk <117369013+OleksandrDanchuk@users.noreply.github.com>
2023-03-17 19:39:31 +0200

    finish

--- js/4mod.js
+++ js/4mod.js
@@ -369,3 +369,102 @@
 // ];
 // const authorsInAlphabetOrder = [...authors].sort((a, b) => a.localeCompare(b));
 // const authorsInReversedOrder = [...authors].sort((a, b) => b.localeCompare(a));
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+// ];
+// const sortedByAuthorName = [...books].sort((a, b) =>
+//   a.author.localeCompare(b.author))
+// ;
+// const sortedByReversedAuthorName = [...books].sort((a, b) =>
+//   b.author.localeCompare(a.author))
+// ;
+// const sortedByAscendingRating = [...books].sort((a, b) =>
+//   a.rating - b.rating)
+// ;
+// const sortedByDescentingRating = [...books].sort((a, b) =>
+//   b.rating - a.rating)
+// ;
+
+// const sortByAscendingBalance = users => {
+//   [...users].sort(
+//     (firstUser, secondUser) => firstUser.balance - secondUser.balance
+//   );
+// };
+
+// const sortByDescendingFriendCount = users =>
+//   [...users].sort(
+//     (firstUser, secondUser) =>
+//       secondUser.friends.length - firstUser.friends.length
+//   );
+
+// const sortByName = users => {
+//   return [...users].sort((a, b) => a.name.localeCompare(b.name));
+// };
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   {
+//     title: 'The Dreams in the Witch House',
+//     author: 'Howard Lovecraft',
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
+// const names = [...books]
+//   .filter(book => book.rating > MIN_BOOK_RATING)
+//   .sort((a, b) => a.author.localeCompare(b.author))
+//   .map(book => book.author);
+
+// const getNamesSortedByFriendCount = users => {
+//   return [...users]
+//     .sort(
+//       (firstFriend, secondFriend) =>
+//         firstFriend.friends.length - secondFriend.friends.length
+//     )
+//     .map(user => user.name);
+// };
+
+// const getSortedFriends = users =>
+//   [...users]
+//     .flatMap(user => user.friends)
+//     .filter((friend, index, array) => array.indexOf(friend) === index)
+//     .sort((firstFriend, secondFriend) =>
+//       firstFriend.localeCompare(secondFriend)
+//     );
+
+// const getTotalBalanceByGender = (users, gender) =>
+//   [...users]
+//     .filter(user => user.gender === gender)
+//     .reduce((acc, user) => acc + user.balance, 0);
